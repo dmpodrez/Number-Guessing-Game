83d61f06e291a87788ce3564db33eadc3cb8c79f
Dmitry Podrez <dima.padrez.00@icloud.com>
2024-09-16 22:16:03 +0200

    modified:   add count of attempts

--- guessing-code.py
+++ guessing-code.py
@@ -1,18 +1,22 @@
 import random 
 def guess(key_num):
+  count = 0 
   while True:
     n = int(input())
     print('Your number:', n) 
     if n > key_num:
       print('Too much, try again')
+      count += 1
     elif n < key_num:
       print('Too few, try again')
+      count += 1
     else:
       print('You guessed, congratulations!')
+      print('You guessed the number in', count, 'attempts')
       break
 
 from_num = int(input())
 to_num = int(input())
 key_num = random.randint(from_num, to_num) 
 
-print(guess(key_num))
+guess(key_num)
